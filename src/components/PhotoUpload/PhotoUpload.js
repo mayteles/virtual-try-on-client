3639cf5e9359a90c9a7f1d8cecd58adf888a19a2
Falak Zahra <falak.zahra16@gmail.com>
2024-04-20 14:24:35 -0600

    create a new photoUpload component and add the example photo

---
+++ src/components/PhotoUpload/PhotoUpload.js
@@ -0,0 +1,19 @@
+import "./PhotoUpload.scss";
+const PhotoUpload = () => {
+  return (
+    <>
+      <div className="photo-upload-section">
+        <div>
+          <p className="photo-caption">photo example</p>
+          <div className="photo photo__example" src="" alt=""></div>
+        </div>
+        <div>
+          <p className="photo-caption">silhouette in creation</p>
+          <div className="photo" src="" alt=""></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PhotoUpload;
