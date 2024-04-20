ce9baaae5cb326afac07328ecdffe981b0509a9f
Falak Zahra <falak.zahra16@gmail.com>
2024-04-20 15:42:43 -0600

    Create Silhouette component and apply animation to change images using state

--- src/components/PhotoUpload/PhotoUpload.js
+++ src/components/PhotoUpload/PhotoUpload.js
@@ -1,15 +1,33 @@
+import React, { useState } from "react";
 import "./PhotoUpload.scss";
+import { Silhouette } from "../Silhouette/Silhouette";
+
 const PhotoUpload = () => {
+  const [silhouetteState, setSilhouetteState] = useState("figure-only");
+
+  const changeState = () => {
+    switch (silhouetteState) {
+      case "figure-only":
+        setSilhouetteState("silhouette");
+        break;
+      case "silhouette":
+        setSilhouetteState("colored");
+        break;
+      default:
+        setSilhouetteState("figure-only");
+    }
+  };
+
   return (
     <>
       <div className="photo-upload-section">
         <div>
-          <p className="photo-caption">photo example</p>
-          <div className="photo photo__example" src="" alt=""></div>
+          <p className="photo-caption">Photo example</p>
+          <div className="photo photo__example"></div>
         </div>
-        <div>
-          <p className="photo-caption">silhouette in creation</p>
-          <div className="photo" src="" alt=""></div>
+        <div onClick={changeState}>
+          <p className="photo-caption">Silhouette in creation</p>
+          <Silhouette state={silhouetteState} />
         </div>
       </div>
     </>
