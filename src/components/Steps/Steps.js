e81445f2cf383a381be3d07d8b61f2f6424d7577
Rebecca Vinet <rebecca.vinet@outlook.com>
2024-04-21 18:53:01 -0400

    completed top part for step 1

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -7,15 +7,17 @@ function Steps() {
         <div className="steps__div">
             <div className="steps__container">
                 <p className="steps__number-one">1</p>          
+                <span className="steps__line"></span>
                 <p className="steps__number">2</p>
+                <span className="steps__line"></span>
                 <p className="steps__number">3</p>
             </div>
-            <p className="steps__explain">Upload a photo of yourself and we'll make a silhouette of it</p>
             <div className="steps__path">
                 <svg xmlns="http://www.w3.org/2000/svg" width="9" height="5" viewBox="0 0 9 5" fill="none">
                     <path d="M4.5 0L8.39711 4.5H0.602886L4.5 0Z" fill="#67707D"/>
                 </svg>
             </div>
+            <p className="steps__explain">Upload a photo of yourself and we'll make a silhouette of it</p>
         </div>
       </section>
     );
