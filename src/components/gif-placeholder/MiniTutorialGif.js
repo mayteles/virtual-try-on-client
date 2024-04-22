2fd10c70270dbb5f37e0ae31a19a8f5a5584a729
mayteles <maylon.ferreira@icloud.com>
2024-04-22 10:25:23 -0700

    style implemented

--- src/components/gif-placeholder/MiniTutorialGif.js
+++ src/components/gif-placeholder/MiniTutorialGif.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import gifguide from '../../assets/GIF/ezgif-4-696ec24dd8.gif'
-
+import React from 'react';
+import gifguide from '../../assets/GIF/ezgif-4-696ec24dd8.gif';
 
 function MiniTutorialGif() {
   return (
     <div className='mini'>
       <p className='mini__p'>Welcome to try our virtual fitting AI! View clothes on a model with a shape like yours to make sure they fit just right.</p>
-      <section className='mini__img-box'>
-          <img src={gifguide} alt="guide step"/>
-      </section>
-      <button className='mine__cta'>Try it out</button>
-      <button>Cancel</button>
+      <div className='mini__img-container'>
+        <img className='mini__img-box' src={gifguide} alt="guide step"/>
+        <div className='mini__buttons'>
+          <button className='mini__cta'>Try it out</button>
+          <br/> {/* Add a line break to move the next button to the next row */}
+          <button className='mini__cancel'>Cancel</button>
+        </div>
+      </div>
     </div>
   )
 }
