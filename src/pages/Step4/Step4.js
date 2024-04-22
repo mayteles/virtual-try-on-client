3dde294d1c56dfe9249f0892a2e37e2f44cf0a9b
mayteles <maylon.ferreira@icloud.com>
2024-04-21 19:16:45 -0700

    linking path for step4

---
+++ src/pages/Step4/Step4.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MiniTutorialGif from '../../components/gif-placeholder/MiniTutorialGif'
+
+function Step4() {
+  return (
+    <div>
+      <MiniTutorialGif />
+    </div>
+  )
+}
+
+export default Step4
