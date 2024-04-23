b99d8cfa649acd858a5eeb1230c0e3b73a535f9a
Rebecca Vinet <rebecca.vinet@outlook.com>
2024-04-22 23:18:10 -0400

    updated spacing on mini tutorial gif page

--- src/components/gif-placeholder/MiniTutorialGif.js
+++ src/components/gif-placeholder/MiniTutorialGif.js
@@ -5,7 +5,7 @@ import gifguide from '../../assets/GIF/ezgif-tutorial v3 Apr 22.gif';
 function MiniTutorialGif() {
   return (
     <div className='mini'>
-      <p className='mini__p'>Welcome to try our virtual fitting AI! View clothes on a model with a shape like yours to make sure they fit just right.</p>
+      <p className='mini__p'>Welcome to our virtual fitting AI! View clothes on a model with a shape like yours to make sure they fit just right.</p>
       <div className='mini__img-container'>
         <img className='mini__img-box' src={gifguide} alt="guide step"/>
         <div className='mini__buttons'>
